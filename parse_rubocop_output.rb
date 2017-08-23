88d0027aaacf832d139d803e3b3c5e4d22f65d7e
odawara <odawara@hyakuren.org>
2017-08-23 10:44:47 +0900

    Metrics周りの値についてサポート

--- parse_rubocop_output.rb
+++ parse_rubocop_output.rb
@@ -11,9 +11,10 @@ require "json"
 require "csv"
 
 # 2次元配列を転置する
+# Array#transposeだと配列サイズの不一致によるエラーが発生するので自前で実装
 # @param [Array<Array>] array
 # @return 転置された配列
-def transpose(array)
+def transpose_f(array)
   transposed_array = []
   array.each.with_index do |row, row_no|
     row.each.with_index do |_, col_no|
@@ -29,8 +30,17 @@ end
 # @param [Hash] h 配列offensesの各ハッシュ要素
 # @return [String] 発生位置情報
 def file_info(path, h)
-  "#{h["severity"][0,1].upcase}: " +
+  info = "#{h["severity"][0,1].upcase}: " +
     "#{path}(#{h["location"]["line"]},#{h["location"]["column"]}) "
+  m = h["message"].match(/\[[0-9]+(\.[0-9]+)?\/[0-9]+\]/)
+  info += m[0] if m
+  info
+end
+
+# 数値的なメトリクス情報のみ取得
+def get_metrics(h)
+  m = h["message"].match(/\[([0-9]+(\.[0-9]+)?)\/[0-9]+\]/)
+  m ? m[1] : nil
 end
 
 # 変数の定義
@@ -60,31 +70,40 @@ res["files"].each do |t_f|
       offenses[cop_name] = {
         count: 1,
         files: [file_info(path, o)],
+        metrics: [get_metrics(o)],
         severity: o["severity"],
         message: o["message"],
       }
     else
       offenses[cop_name][:count] += 1
       offenses[cop_name][:files] << file_info(path, o)
+      offenses[cop_name][:metrics] << get_metrics(o)
     end
   end
 end
 
-rows = [["Cop Name", "Severity", "Message", "Count", "Path & Location"]]
+rows = [["Cop Name", "Severity", "Message", "Count"]]
 offenses.each do |cop_name, h|
   rows << [cop_name, h[:severity].upcase, h[:message], h[:count]]
 end
-cols1 = transpose(rows)
+cols1 = transpose_f(rows)
 
-rows = []
+rows = [["Path & Location"]]
 offenses.each do |_, h|
   rows << h[:files]
 end
-cols2 = transpose(rows)
+cols2 = transpose_f(rows)
+
+rows = [["Metrics"]]
+offenses.each do |_, h|
+  rows << h[:metrics]
+end
+cols3 = transpose_f(rows)
 
 CSV.open(OUTPUT_FILE, "wb") do |csv|
   cols1.each { |row| csv << row }
   cols2.each { |row| csv << row }
+  cols3.each { |row| csv << row }
 end
 
 puts "'#{OUTPUT_FILE}' is created successfully!"
